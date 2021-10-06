25f368e0eef13ac76da04ac01eee2ddc8941c879
신승민 <sky2197kr@gmail.com>
2021-10-07 03:01:55 +0900

    임시 header footer 추가

---
+++ src/components/_Common/header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="page-header">header</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
---
+++ src/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Header />
+    <Nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/_Common/header'
+import Footer from '@/components/_Common/footer'
+export default {
+  components: { Footer, Header },
+}
+</script>
+
+<style>
+</style>
