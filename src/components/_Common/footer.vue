e7f1c67eade51d3cdcb41d54c84059ee1588084c
신승민 <sky2197kr@gmail.com>
2021-10-09 18:05:17 +0900

    footer 메뉴 이름 추가

--- src/components/_Common/footer.vue
+++ src/components/_Common/footer.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="page-footer">
-    <div>
+    <div class="footer-col">
       <font-awesome-icon icon="home" style="font-size: 20px" />
+      <div>대시보드</div>
     </div>
-    <div>
+    <div class="footer-col">
       <font-awesome-icon icon="band-aid" style="font-size: 20px" />
+      <div>접종후기</div>
     </div>
-    <div>
+    <div class="footer-col">
       <font-awesome-icon icon="clipboard" style="font-size: 20px" />
+      <div>게시판</div>
     </div>
   </div>
 </template>
