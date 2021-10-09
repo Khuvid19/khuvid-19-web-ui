3b061ab2682f2f95fbee5f1aea23e4c5fb59d376
신승민 <sky2197kr@gmail.com>
2021-10-09 18:26:48 +0900

    header layout 구현

--- src/components/_Common/header.vue
+++ src/components/_Common/header.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="page-header">header</div>
+  <div class="page-header">
+    <div class="header-logo">KHUVID(로고)</div>
+    <div class="header-user btn btn-xs btn-primary">LogIn</div>
+  </div>
 </template>
 
 <script>
