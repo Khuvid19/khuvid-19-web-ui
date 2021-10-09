5abbc447c87805ed535852f71f13afadc95c4a3d
신승민 <sky2197kr@gmail.com>
2021-10-09 18:31:43 +0900

    페이지 및 컴포넌트 공통 세팅

---
+++ src/components/Board/test.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div class="text-purple-600">테스트 컴포넌트</div>
+    <button class="btn btn-primary">버튼</button>
+    <a class="btn">Hello!</a>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ src/pages/review/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <TestComponent />
+    <button class="btn btn-primary">버튼</button>
+  </div>
+</template>
+
+<script>
+import TestComponent from '@/components/Main/test'
+
+export default {
+  components: { TestComponent },
+}
+</script>
