b0db26e0b42b12ddf8b0bc1f538e49a9c9079c5b
ChoiByeol <honey333888@naver.com>
2021-10-29 21:47:59 +0900

    접종자 수 표시

--- src/components/Main/dashboard.vue
+++ src/components/Main/dashboard.vue
@@ -3,12 +3,16 @@
     <div class="flex flex-col w-full">
       <div class="dashboard-logo">상황판</div>
       <div class="grid h-20 card bg-base-300 rounded-box place-items-center">확진자</div> 
-      <div class="divider"></div>
+      <div class="divline"></div>
       <div class="grid h-20 card bg-base-300 rounded-box place-items-center">
         <div class="flex flex-row w-full">
-          <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">1차 접종</div> 
-          <div class="divline divider divider-vertical"></div> 
-          <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">접종 완료</div>
+          <div class="inoculator-area grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">
+            1차 접종<br>{{ first }}
+          </div> 
+          <div class="hidden divider divider-vertical"></div> 
+          <div class="inoculator-area grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">
+            2차 접종<br>{{ second }}
+          </div>
         </div>
       </div>
     </div>
@@ -31,10 +35,10 @@ export default {
   created () {
     const PATH_API = '/irgd/cov19stats.do?list=all'
     axios.get(`/api/v1${PATH_API}`).then(res => {
-      console.log(res)
       const xml = res.data
-      const json = convert.xml2json(xml, { compact: true })
-      console.log(json)
+      const json = JSON.parse(convert.xml2json(xml, { compact: true }))
+      this.first = json.response.body.items.item[2].firstCnt._text
+      this.second = json.response.body.items.item[2].secondCnt._text
     })
   },
 }
