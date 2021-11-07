2fa6717e6a19232ae8687fd11f0ead2244745dc5
YejiKim <sso06030@gmail.com>
2021-11-07 19:18:46 +0900

    v-calendar plugin 추가

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,6 +31,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '@/plugins/chartJs/index', mode: 'client' },
+    { src: '@/plugins/calendar/vCalendar.js', ssr: false },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
