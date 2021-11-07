40abe5cd2e896d16e0728c27cf439bf5cb4b001b
YejiKim <sso06030@gmail.com>
2021-11-07 17:10:50 +0900

    간편후기 등록 마크업

---
+++ src/components/Review/addSimpleCont.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  sdfs
+</div>
+</template>
+
+<script>
+export default {
+  name: "AddSimpleCont",
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/review/index.vue
+++ src/pages/review/index.vue
@@ -1,5 +1,54 @@
 <template>
   <div>
+    <label for="my-modal-2" class="btn btn-primary modal-button">open modal</label>
+    <input id="my-modal-2" type="checkbox" class="modal-toggle">
+    <div class="modal items-center">
+      <div class="modal-box w-11/12 rounded-box px-0 py-2 h-auto" >
+        <div class="card bordered px-3 py-4 mx-4 my-2 bg-white">
+          <div class="text-lg mb-1 ml-1">백신 종류</div>
+          <div>
+            <button v-for="(item,idx) in vaccineList" :key="idx"
+                    class="m-1 btn btn-outline btn-primary btn-sm">
+              {{ item }}
+            </button>
+          </div>
+        </div>
+        <div class="card bordered px-3 py-4 mx-4 my-2 bg-white">
+          <div class="text-lg mb-1 ml-1">접종 날짜</div>
+          <div>
+            <button v-for="(item,idx) in vaccineList" :key="idx"
+                    class="m-1 btn btn-outline btn-primary btn-sm">
+              {{ item }}
+            </button>
+          </div>
+        </div>
+        <div class="card bordered px-3 py-4 mx-4 my-2 bg-white">
+          <div class="text-lg mb-1 ml-1">기저질환</div>
+          <div>
+            <button v-for="(item,idx) in bb" :key="idx"
+                    class=" m-1 btn btn-outline btn-primary btn-sm">
+              {{ item }}
+            </button>
+          </div>
+          <div class="w-full my-2 h-px bg-gray-200"></div>
+          <div class="form-control">
+            <input type="text" placeholder="username" class="input input-sm input-bordered">
+          </div>
+        </div>
+        <div class="card bordered px-3 py-4 mx-4 my-2 bg-white">
+          <div class="text-lg mb-1 ml-1">이상반응</div>
+          <div>
+            <button v-for="(item,idx) in cc" :key="idx"
+                    class="m-1 btn btn-outline btn-primary btn-sm">
+              {{ item }}
+            </button>
+          </div>
+        </div>
+        <div class="modal-action w-full px-4 my-3">
+          <label for="my-modal-2" class="btn btn-primary w-full rounded-xl">작성완료</label>
+        </div>
+      </div>
+    </div>
     <search-filter @clickFilter="moveToScreen('filter')"/>
     <div class="w-full my-2 h-px bg-gray-200"></div>
     <div class="board-list overflow-y-scroll">
@@ -24,7 +73,7 @@
     >
       <div class="flex justify-end items-center gap-4">
         <button class="text-white text-xl bg-primary w-40 h-16 rounded-xl"
-                @click="moveToScreen('addSimple')">
+                @click="openSimple">
           간편후기
         </button>
         <button class="text-white text-xl bg-primary w-40 h-16 rounded-xl"
@@ -47,20 +96,21 @@
       <add-detail-cont v-if="screenType === 'addDetail'"/>
       <detail-cont v-if="screenType === 'detail'"/>
     </FullScreen>
-
+    <add-simple-cont v-model="simpleFlag"/>
   </div>
 </template>
 
 <script>
 import SearchFilter from '@/components/Review/searchFilter'
 import ListCont from '@/components/Review/listCont'
-import AddDetailCont from '@/components/Review/addCont'
+import AddDetailCont from '@/components/Review/addDetailCont'
+import AddSimpleCont from '@/components/Review/addSimpleCont'
 import DetailCont from '@/components/Review/detailCont'
 import FilterCont from '@/components/Review/filterCont'
 import FullScreen from '@/components/_Common/fullScreen'
 
 export default {
-  components: {DetailCont,AddDetailCont, FilterCont, FullScreen, SearchFilter, ListCont},
+  components: {AddSimpleCont, DetailCont, AddDetailCont, FilterCont, FullScreen, SearchFilter, ListCont},
   data() {
     return {
       screenType: null,
@@ -68,16 +118,25 @@ export default {
       screenTitle: '',
       screenOkText: '',
       btnFlag: false,
+      simpleFlag: false,
+      vaccineList: ['화이자', '모더나'],
+      bb: ['없음', '있음'],
+      cc: [
+        '미열', '고열', '접종부위 통증',
+        '구토', '메스꺼움', '두통',
+        '관절통', '근육통', '피로감',
+        '두드러기', '발진', '가려움증',
+        '부기', '기타',
+      ],
     }
   },
   methods: {
+    openSimple() {
+      this.simpleFlag = true;
+    },
     moveToScreen(type) {
       this.screenType = type
       switch (type) {
-        case 'addSimple':
-          this.screenTitle = ''
-          this.screenOkText = '완료'
-          break
         case 'addDetail':
           this.screenTitle = '접종후기'
           this.screenOkText = '완료'
