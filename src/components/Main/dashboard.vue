142829fd94b0387f3287a39cd742c94e2d8cc5ea
ChoiByeol <honey3388@khu.ac.kr>
2021-11-13 15:24:45 +0900

    숫자 쉼표 추가, 퍼센트 추가

--- src/components/Main/dashboard.vue
+++ src/components/Main/dashboard.vue
@@ -20,14 +20,14 @@
       <div class="status-border grid h-20 card bg-base-300 rounded-box place-items-center">
         <div class="flex flex-row w-full">
           <div class="inoculator-area text-sm grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">
-            1차 접종<br>{{ first }}<br>(퍼센트)
+            1차 접종<br>{{ first }}<br>({{ firstPercent }}%)
           </div> 
           <div class="hidden divider divider-vertical"></div> 
           <div class="inoculator-area text-sm grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">
-            2차 접종<br>{{ second }}<br>(퍼센트)
+            2차 접종<br>{{ second }}<br>({{ secondPercent }}%)
           </div>
           <div class="inoculator-area text-sm grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">
-            3차 접종<br>{{ third }}<br>(퍼센트)
+            3차 접종<br>{{ third }}<br>({{ thirdPercent }}%)
           </div>
         </div>
       </div>
@@ -56,11 +56,18 @@ export default {
   created () {
     const PATH_API = '/irgd/cov19stats.do?list=all'
     axios.get(`/api/v1${PATH_API}`).then(res => {
+      const internationalNumberFormat = new Intl.NumberFormat('en-US')
       const xml = res.data
       const json = JSON.parse(convert.xml2json(xml, { compact: true }))
       this.first = json.response.body.items.item[2].firstCnt._text
       this.second = json.response.body.items.item[2].secondCnt._text
       this.third = json.response.body.items.item[2].thirdCnt._text
+      this.firstPercent = (this.first / 51667688 * 100).toFixed(1);
+      this.secondPercent = (this.second / 51667688 * 100).toFixed(1);
+      this.thirdPercent = (this.third / 51667688 * 100).toFixed(1);
+      this.first = internationalNumberFormat.format(this.first);
+      this.second = internationalNumberFormat.format(this.second);
+      this.third = internationalNumberFormat.format(this.third);
     })
   },
 }
