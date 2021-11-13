1f22bdd5281a70f88674f55b06c59a92f2c0559e
신승민 <sky2197kr@gmail.com>
2021-11-13 19:19:37 +0900

    drawerSide 컴포넌트 분리

---
+++ src/components/_Common/drawerSide.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="drawer-side">
+    <label for="my-drawer-4" class="drawer-overlay">이름</label>
+    <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+      <li>
+        <a>내 정보</a>
+      </li>
+      <li>
+        <a>작성 후기</a>
+      </li>
+      <li>
+        <a>내가 쓴 글</a>
+      </li>
+      <li>
+        <a @click="clickLogout">로그아웃</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    clickLogout() {
+      this.$auth.logout()
+      this.$router.go()
+    },
+  },
+}
+</script>
+
+<style>
+</style>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -7,23 +7,7 @@
       <Footer />
       <SignUp />
     </div>
-    <div class="drawer-side">
-      <label for="my-drawer-4" class="drawer-overlay">이름</label>
-      <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-        <li>
-          <a>내 정보</a>
-        </li>
-        <li>
-          <a>작성 후기</a>
-        </li>
-        <li>
-          <a>내가 쓴 글</a>
-        </li>
-        <li>
-          <a @click="clickLogout">로그아웃</a>
-        </li>
-      </ul>
-    </div>
+    <DrawerSide />
   </div>
 </template>
 
@@ -31,17 +15,13 @@
 import SignUp from '../components/SignUp/signUp'
 import Header from '@/components/_Common/header'
 import Footer from '@/components/_Common/footer'
+import DrawerSide from '@/components/_Common/drawerSide'
 export default {
-  components: { Footer, Header, SignUp },
+  components: { Footer, Header, SignUp, DrawerSide },
   created() {
     if (this.$route.hash !== '') this.$router.replace('/main')
   },
-  methods: {
-    clickLogout() {
-      this.$auth.logout()
-      this.$router.go()
-    },
-  },
+  methods: {},
 }
 </script>
 
