017a5e12b33c513f39cb891cddf3c7baa83123f3
신승민 <sky2197kr@gmail.com>
2021-11-22 19:38:27 +0900

    fullScreen 메뉴 추가

--- src/components/_Common/fullScreen.vue
+++ src/components/_Common/fullScreen.vue
@@ -5,14 +5,32 @@
   >
     <header class="grid grid-cols-3 items-center h-16 shadow">
       <fa-icon
-        class="ml-5 text-2xl cursor-pointer"
+        class="ml-5 text-2xl cursor-pointer self-center"
         icon="arrow-left"
         @click="onClickBack"
       />
       <div class="text-xl text-center">{{ title }}</div>
-      <div class="text-right mr-5 cursor-pointer" @click="onClickSideBtn">
-        {{ sideBtnText }}
+      <div
+        v-if="menuList.length !== 0"
+        class="dropdown dropdown-end text-right pr-5"
+      >
+        <fa-icon
+          tabindex="0"
+          class="text-2xl cursor-pointer padding-5"
+          :icon="['fas', 'ellipsis-v']"
+        />
+        <ul
+          tabindex="0"
+          class="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52"
+        >
+          <li v-for="menu in menuList" :key="menu">
+            <a @click="onClickMenu(menu)">{{ menu }}</a>
+          </li>
+        </ul>
       </div>
+      <!-- <div class="text-right mr-5 cursor-pointer" @click="onClickSideBtn">
+        {{ sideBtnText }}
+      </div> -->
     </header>
     <main
       :class="`w-full overflow-y-scroll ${
@@ -61,6 +79,10 @@ export default {
       default: null,
       type: String,
     },
+    menuList: {
+      type: Array,
+      default: () => [],
+    },
   },
   methods: {
     onClickBack() {
@@ -71,6 +93,9 @@ export default {
     },
     onClickOk() {
       this.$emit('onClickOk')
+    },
+    onClickMenu(menu) {
+      this.$emit('onClickMenu', menu)
     },
   },
 }
