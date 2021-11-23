640808b6782934a13fe2698d55a64e35d5724606
신승민 <sky2197kr@gmail.com>
2021-11-23 12:19:33 +0900

    글쓰기 구현 및 detail write screen 분리

--- src/components/Board/Screen/detailScreen/detailScreen.vue
+++ src/components/Board/Screen/detailScreen/detailScreen.vue
@@ -1,36 +1,49 @@
 <template>
-  <div class="h-full">
-    <DetailContent />
-    <div
-      class="mt-4 mb-2 w-full bg-gray-300 rounded-full"
-      style="height: 1px"
-    ></div>
-    <div class="overflow-y-hidden">
-      <template v-for="(el, idx) in [0, 1, 2, 3, 4, 5, 6, 7, 8]">
-        <CommentItem :key="idx" />
-        <div
-          v-if="idx !== [0, 1, 2, 3, 4, 5, 6, 7, 8].length - 1"
-          :key="`${idx}i`"
-          class="pl-2 pr-2"
-        >
+  <full-screen v-model="screenFlag" title="게시글" @onClickBack="onClickBack">
+    <div class="h-full">
+      <DetailContent />
+      <div
+        class="mt-4 mb-2 w-full bg-gray-300 rounded-full"
+        style="height: 1px"
+      ></div>
+      <div class="overflow-y-hidden">
+        <template v-for="(el, idx) in [0, 1, 2, 3, 4, 5, 6, 7, 8]">
+          <CommentItem :key="idx" />
           <div
-            class="mt-2 mb-2 w-full bg-gray-200 rounded-full"
-            style="height: 1px"
-          ></div>
-        </div>
-      </template>
+            v-if="idx !== [0, 1, 2, 3, 4, 5, 6, 7, 8].length - 1"
+            :key="`${idx}i`"
+            class="pl-2 pr-2"
+          >
+            <div
+              class="mt-2 mb-2 w-full bg-gray-200 rounded-full"
+              style="height: 1px"
+            ></div>
+          </div>
+        </template>
+      </div>
+      <CommentInput />
     </div>
-    <CommentInput />
-  </div>
+  </full-screen>
 </template>
 
 <script>
 import DetailContent from './detailContent'
 import CommentInput from './commentInput'
 import CommentItem from './commentItem'
+import FullScreen from '@/components/_Common/fullScreen'
 export default {
   name: 'BoardDetailScreen',
-  components: { CommentInput, CommentItem, DetailContent },
+  components: { CommentInput, CommentItem, DetailContent, FullScreen },
+  data() {
+    return {
+      screenFlag: false,
+    }
+  },
+  methods: {
+    onClickBack() {
+      this.screenFlag = false
+    },
+  },
 }
 </script>
 
--- src/components/Board/Screen/writeScreen/writeScreen.vue
+++ src/components/Board/Screen/writeScreen/writeScreen.vue
@@ -1,45 +1,74 @@
 <template>
-  <div class="pl-4 pr-4 pt-4 h-fll">
-    <input
-      v-model="title"
-      placeholder="제목을 입력해주세요."
-      class="
-        rounded-lg
-        border-2
-        focus:border-primary
-        outline-none
-        p-1
-        pl-2
-        w-full
-        mb-2
-      "
-    />
-    <textarea
-      v-model="content"
-      placeholder="내용을 입력해주세요."
-      class="
-        border-2
-        p-2
-        focus:border-primary
-        outline-none
-        w-full
-        rounded-lg
-        h-auto
-        write-textarea
-        resize-none
-      "
-    />
-  </div>
+  <full-screen
+    v-model="screenFlag"
+    title="글쓰기"
+    ok-text="완료"
+    @onClickBack="onClickBack"
+    @onClickOk="onClickOk"
+  >
+    <div class="pl-4 pr-4 pt-4 h-fll">
+      <input
+        v-model="title"
+        placeholder="제목을 입력해주세요."
+        class="
+          rounded-lg
+          border-2
+          focus:border-primary
+          outline-none
+          p-1
+          pl-2
+          w-full
+          mb-2
+        "
+      />
+      <textarea
+        v-model="content"
+        placeholder="내용을 입력해주세요."
+        class="
+          border-2
+          p-2
+          focus:border-primary
+          outline-none
+          w-full
+          rounded-lg
+          h-auto
+          write-textarea
+          resize-none
+        "
+      />
+    </div>
+  </full-screen>
 </template>
 
 <script>
+import fullScreen from '@/components/_Common/fullScreen'
 export default {
   name: 'BoardWriteScreen',
+  components: { fullScreen },
   data() {
     return {
+      screenFlag: false,
       title: '',
       content: '',
     }
+  },
+  methods: {
+    onClickBack() {
+      this.title = ''
+      this.content = ''
+      this.screenFlag = false
+    },
+    onClickOk() {
+      const params = {
+        title: this.title,
+        content: this.content,
+      }
+
+      this.$axios.post('board', params).then(() => {
+        this.$emit('afterWrite')
+        this.onClickBack()
+      })
+    },
   },
 }
 </script>
