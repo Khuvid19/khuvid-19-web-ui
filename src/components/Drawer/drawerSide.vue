b5f7230beb9fbd933d805eb85ed4fd4ddb97fd07
신승민 <sky2197kr@gmail.com>
2021-11-25 23:37:18 +0900

    drawerSide 폴더 변경

--- src/components/Drawer/drawerSide.vue
+++ src/components/Drawer/drawerSide.vue
@@ -18,20 +18,22 @@
         </div>
       </div>
       <li class="mb-2">
-        <a @click="clickMenu('myInfo')"
-          ><fa-icon class="text-2xl mr-6" :icon="['fas', 'user']" />내 정보</a
-        >
+        <a @click="clickMenu('myInfo')">
+          <fa-icon class="text-2xl mr-6" :icon="['fas', 'user']" />
+          내 정보
+        </a>
       </li>
       <li class="mb-2">
-        <a @click="clickMenu('myReview')"
-          ><fa-icon class="text-2xl mr-5" :icon="['fas', 'edit']" />작성 후기</a
-        >
+        <a @click="clickMenu('myReview')">
+          <fa-icon class="text-2xl mr-5" :icon="['fas', 'edit']" />
+          내가 쓴 후기
+        </a>
       </li>
       <li>
-        <a @click="clickMenu('myPost')"
-          ><fa-icon class="text-2xl mr-7" :icon="['fas', 'file-alt']" />내가 쓴
-          글</a
-        >
+        <a @click="clickMenu('myPost')">
+          <fa-icon class="text-2xl mr-7" :icon="['fas', 'file-alt']" />
+          내가 쓴 글
+        </a>
       </li>
       <li class="bottom-5 absolute">
         <a @click="clickLogout"
@@ -42,7 +44,8 @@
         >
       </li>
     </ul>
-    <MyInfoScreen ref="myInfoScreen" />
+    <sign-up ref="myInfoScreen" mode="myInfo" />
+    <MyInfoScreen />
     <my-review-screen ref="myReviewScreen" />
     <my-post-screen ref="myPostScreen" />
   </div>
@@ -50,12 +53,13 @@
 
 <script>
 import { mapGetters } from 'vuex'
-import MyInfoScreen from '../Drawer/myInfoScreen'
-import MyPostScreen from '../Drawer/myPostScreen'
-import MyReviewScreen from '../Drawer/myReviewScreen'
+import SignUp from '../SignUp/signUp'
+import MyInfoScreen from './myInfoScreen'
+import MyPostScreen from './myPostScreen'
+import MyReviewScreen from './myReviewScreen'
 
 export default {
-  components: { MyInfoScreen, MyReviewScreen, MyPostScreen },
+  components: { MyInfoScreen, MyReviewScreen, MyPostScreen, SignUp },
   computed: {
     ...mapGetters({
       user: 'getUser',
@@ -72,9 +76,8 @@ export default {
     clickMenu(menu) {
       switch (menu) {
         case 'myInfo':
-          console.log('this.user', this.user)
-
-          // this.$refs.myInfoScreen.myInfoScreenFlag = true
+          console.log('1234')
+          this.$refs.myInfoScreen.screenFlag = true
           break
         case 'myReview':
           this.$refs.myReviewScreen.screenFlag = true
