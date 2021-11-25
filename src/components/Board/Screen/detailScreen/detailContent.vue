a5f541fd0acc0493dd704ca6756d0affb787605d
신승민 <sky2197kr@gmail.com>
2021-11-25 22:29:03 +0900

    item formatDate 추가

--- src/components/Board/Screen/detailScreen/detailContent.vue
+++ src/components/Board/Screen/detailScreen/detailContent.vue
@@ -7,7 +7,7 @@
       />
       <div class="ml-2">
         <div class="text-sm">{{ nickname }}</div>
-        <div class="text-2xs text-gray-400">{{ date ? date : 12345 }}</div>
+        <div class="text-2xs text-gray-400">{{ mm_formatDate(date) }}</div>
       </div>
     </div>
     <div class="font-bold text-lg mb-2">{{ title }}</div>
