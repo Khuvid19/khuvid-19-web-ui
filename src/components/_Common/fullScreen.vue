9073ff62021fc77fff2a33908c9ada06723a4548
신승민 <sky2197kr@gmail.com>
2021-11-28 02:05:32 +0900

    fullScreen v-if 변경 및 css 추가

--- src/components/_Common/fullScreen.vue
+++ src/components/_Common/fullScreen.vue
@@ -1,63 +1,65 @@
 <template>
-  <div
-    v-show="value"
-    class="fullScreen transition-all fixed top-0 w-full h-full z-50 bg-white"
-  >
-    <header class="grid grid-cols-3 items-center h-16 shadow">
-      <fa-icon
-        class="ml-5 text-2xl cursor-pointer self-center"
-        icon="arrow-left"
-        @click="onClickBack"
-      />
-      <div class="text-xl text-center">{{ title }}</div>
-      <div
-        v-if="menuList.length !== 0"
-        class="dropdown dropdown-end text-right pr-5"
-      >
+  <transition name="fade">
+    <div
+      v-if="value"
+      class="fullScreen fixed top-0 w-full h-full z-50 bg-white"
+    >
+      <header class="grid grid-cols-3 items-center h-16 shadow">
         <fa-icon
-          tabindex="0"
-          class="text-2xl cursor-pointer padding-5"
-          :icon="['fas', 'ellipsis-v']"
+          class="ml-5 text-2xl cursor-pointer self-center"
+          icon="arrow-left"
+          @click="onClickBack"
         />
-        <ul
-          tabindex="0"
-          class="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52"
+        <div class="text-xl text-center">{{ title }}</div>
+        <div
+          v-if="menuList.length !== 0"
+          class="dropdown dropdown-end text-right pr-5"
         >
-          <li v-for="menu in menuList" :key="menu">
-            <a @click="onClickMenu(menu)">{{ menu }}</a>
-          </li>
-        </ul>
-      </div>
-      <!-- <div class="text-right mr-5 cursor-pointer" @click="onClickSideBtn">
-        {{ sideBtnText }}
-      </div> -->
-    </header>
-    <main
-      :class="`w-full overflow-y-scroll ${
-        okText !== null ? 'content-area' : 'h-full'
-      }`"
-    >
-      <slot></slot>
-    </main>
-    <footer
-      v-if="okText !== null"
-      class="
-        grid
-        w-screen
-        fixed
-        bottom-0
-        h-16
-        text-center
-        items-center
-        bg-primary
-        cursor-pointer
-        text-white text-2xl
-      "
-      @click="onClickOk"
-    >
-      <div>{{ okText }}</div>
-    </footer>
-  </div>
+          <fa-icon
+            tabindex="0"
+            class="text-2xl cursor-pointer padding-5"
+            :icon="['fas', 'ellipsis-v']"
+          />
+          <ul
+            tabindex="0"
+            class="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li v-for="menu in menuList" :key="menu">
+              <a @click="onClickMenu(menu)">{{ menu }}</a>
+            </li>
+          </ul>
+        </div>
+        <!-- <div class="text-right mr-5 cursor-pointer" @click="onClickSideBtn">
+          {{ sideBtnText }}
+        </div> -->
+      </header>
+      <main
+        :class="`w-full overflow-y-scroll ${
+          okText !== null ? 'content-area' : 'h-full'
+        }`"
+      >
+        <slot></slot>
+      </main>
+      <footer
+        v-if="okText !== null"
+        class="
+          grid
+          w-screen
+          fixed
+          bottom-0
+          h-16
+          text-center
+          items-center
+          bg-primary
+          cursor-pointer
+          text-white text-2xl
+        "
+        @click="onClickOk"
+      >
+        <div>{{ okText }}</div>
+      </footer>
+    </div>
+  </transition>
 </template>
 
 <script>
@@ -102,4 +104,13 @@ export default {
 </script>
 
 <style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: 0.3s;
+}
+
+.fade-enter,
+.fade-leave-to{
+  transform: translateX(100%);
+}
 </style>
