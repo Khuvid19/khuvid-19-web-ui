6fdf6d279a73f79ce4b0520d955c58b39868f157
ChoiByeol <honey3388@khu.ac.kr>
2021-11-30 08:58:05 +0900

    차트 클릭 이벤트 구현

--- src/components/Main/dashboardChart.vue
+++ src/components/Main/dashboardChart.vue
@@ -6,7 +6,8 @@
           <div class="tabs">
             <div class="flex justify-between align-middle">
               <button v-for="(item,idx) in vaccineList" :key="idx"
-                class="mr-2 mb-1 btn btn-outline btn-primary btn-sm">
+                class="mr-2 mb-1 btn btn-outline btn-primary btn-sm"
+                @click="ChangeChart(item)">
                 {{ item }}
               </button>
             </div>
@@ -38,6 +39,7 @@ export default {
         datasets: [],
       },
       chartOptions: null,
+      label: '화이자 1차',
       vaccineList: ['화이자 1차', '화이자 2차', '모더나 1차', '모더나 2차', '아스트라제네카 1차', '아스트라제네카 2차', '얀센', '얀센 부스터샷'],
       symptomList: ['두통', '관절통', '붓기', '고열', '근육통', '부위 통증', '미열', '피로감', '메스꺼움', '두드러기', '발진', '구토', '가려움증', '기타'],
     }
@@ -73,14 +75,19 @@ export default {
         },
       }
     },
-    initData(){
+    initData() {
       this.chartData.labels = this.symptomList
       this.chartData.datasets.push({
-        label: '화이자 1차',
+        label: this.label,
         borderColor: 'rgb(98, 176, 182)',
         backgroundColor: 'rgb(98, 176, 182)',
         data: Array.from( {length: 14}, (_, i) => this.makeRandomNum(100, 300)),
       })
+    },
+    ChangeChart(item) {
+      this.label = item;
+      this.chartData.datasets.pop();
+      this.initData();
     },
   },
 }
