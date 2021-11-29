626a3f9e5a92d74a6e630f38ea8e7719f3bfedcc
ChoiByeol <honey3388@khu.ac.kr>
2021-11-30 08:00:54 +0900

    차트 가로로 변경

--- src/components/Main/dashboardChart.vue
+++ src/components/Main/dashboardChart.vue
@@ -6,14 +6,14 @@
           <div class="tabs">
             <div class="flex justify-between align-middle">
               <button v-for="(item,idx) in vaccineList" :key="idx"
-                class="mr-2 btn btn-outline btn-primary btn-sm">
+                class="mr-2 mb-1 btn btn-outline btn-primary btn-sm">
                 {{ item }}
               </button>
             </div>
           </div>
         </div>
       <bar-chart
-        class="-mt-8 mb-2" style="width: 90%; margin-left: 5%; height:270px;"
+        class="-mt-8 mb-2" style="width: 95%; margin-left: 2.5%; height:400px;"
         :data="chartData"
         :options="chartOptions"
       />
@@ -39,8 +39,7 @@ export default {
       },
       chartOptions: null,
       vaccineList: ['화이자 1차', '화이자 2차', '모더나 1차', '모더나 2차', '아스트라제네카 1차', '아스트라제네카 2차', '얀센', '얀센 부스터샷'],
-      symptomList: ['두통', '관절통', '붓기', '고열', '근육통', '부위 통증', '미열', '피로감', '메스꺼움', '두드러기'],
-      // '발진', '구토', '가려움증', '기타'
+      symptomList: ['두통', '관절통', '붓기', '고열', '근육통', '부위 통증', '미열', '피로감', '메스꺼움', '두드러기', '발진', '구토', '가려움증', '기타'],
     }
   },
   computed: {
@@ -61,6 +60,7 @@ export default {
     initOptions() {
       this.chartOptions = {
         ...defaultOptions,
+        indexAxis: 'y',
         plugins: {
           ...defaultPlugins,
           title: {
@@ -74,7 +74,6 @@ export default {
       }
     },
     initData(){
-      // this.chartData.labels = Array.from( {length: 10}, (_, i) => `증상${i+1}`)
       this.chartData.labels = this.symptomList
       this.chartData.datasets.push({
         label: '화이자 1차',
@@ -82,12 +81,6 @@ export default {
         backgroundColor: 'rgb(98, 176, 182)',
         data: Array.from( {length: 14}, (_, i) => this.makeRandomNum(100, 300)),
       })
-      // this.chartData.datasets.push({
-      //   label: '2차',
-      //   borderColor: '#0ea5e9',
-      //   backgroundColor: '#0ea5e9',
-      //   data:  Array.from( {length: 12}, (_, i) => this.makeRandomNum(200, 400)),
-      // })
     },
   },
 }
