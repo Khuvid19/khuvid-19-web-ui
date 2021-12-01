268cde178ae6b300e3b41a5d8096bcc446eaf19b
ChoiByeol <honey3388@khu.ac.kr>
2021-12-01 20:22:39 +0900

    차트 api 연동

--- src/components/Main/dashboardChart.vue
+++ src/components/Main/dashboardChart.vue
@@ -48,32 +48,16 @@ export default {
       backgroundColor: 'rgb(180, 110, 188)',
       vaccineList: ['화이자 1차', '화이자 2차', '모더나 1차', '모더나 2차', '아스트라제네카 1차', '아스트라제네카 2차', '얀센', '얀센 부스터샷'],
       vaccine: 'PFIZER_FIRST',
+      allCount: 0,
       symptomCode: [],
       symptomList: [],
-      symptomCount: [],
+      symptomCount: {},
     }
   },
-  computed: {
-
-  },
   created() {
+    console.log("야 인마");
+    console.log(this.vaccine);
     this.initChart();
-    const SYMPTOMLIST = '/review/types/sideEffects'
-    axios.get(`/api/v2${SYMPTOMLIST}`).then(res => {
-      const data = res.data
-      console.log(data);
-      for (let i = 0; i < 14; i++) {
-        this.symptomCode.push(data[i].code)
-        this.symptomList.push(data[i].value)
-      }
-      console.log(this.symptomCode);
-      console.log(this.symptomList);
-    })
-    const SYMPTOMCOUNT = `review/sideEffects?vaccine=${this.vaccine}`
-    axios.get(`/api/v2${SYMPTOMCOUNT}`).then(res => {
-      const data2 = res.data
-      console.log(data2);
-    })
   },
   methods: {
     makeRandomNum(min, max){
@@ -92,14 +76,45 @@ export default {
         },
       }
     },
-    initData() {
+    async initData() {
       this.chartData.labels = this.symptomList
+      await this.getAPI()
       this.chartData.datasets.push({
         label: this.label,
         borderColor: this.borderColor,
         backgroundColor: this.backgroundColor,
-        data: Array.from( {length: 14}, (_, i) => this.makeRandomNum(100, 300)),
+        // data: Array.from( {length: 14}, (_, i) => this.makeRandomNum(100, 300)),
+        data: Array.from( {length: 14}, (_, i) => (this.symptomCount[this.symptomCode[i]]/this.allCount) * 100),
       })
+    },
+    async getAPI() {
+      try {
+        this.allCount = 0
+        this.symptomCount = {}
+        this.symptomCode = []
+        this.symptomList = []
+        const SYMPTOMLIST = '/review/types/sideEffects'
+        await axios.get(`/api/v2${SYMPTOMLIST}`).then(res => {
+          const data = res.data
+          for (let i = 0; i < 14; i++) {
+            this.symptomCode.push(data[i].code)
+            this.symptomList.push(data[i].value)
+          }
+        })
+        console.log(this.vaccine);
+        const SYMPTOMCOUNT = `review/sideEffects?vaccine=${this.vaccine}`
+        await axios.get(`/api/v2${SYMPTOMCOUNT}`).then(res => {
+          const data = res.data
+          for (data.key in data) {
+            this.symptomCount[data.key] = data[data.key]
+            this.allCount += data[data.key]
+          }
+          console.log(this.symptomCount);
+          console.log(this.allCount);
+        })
+      } catch (error) {
+        console.log(error);
+      }
     },
     changeBtnColor() {
       if (this.current === 'PFIZER')
@@ -116,25 +131,44 @@ export default {
       if (item.includes("화이자")) {
         // 보라색
         this.current = 'PFIZER'
+        if (item.includes("1차")) {
+          this.vaccine = 'PFIZER_FIRST'
+        } else {
+          this.vaccine = 'PFIZER_SECOND'
+        }
         this.borderColor = 'rgb(180, 110, 188)'
         this.backgroundColor = 'rgb(180, 110, 188)' 
       }
       else if (item.includes("모더나")) {
         // 빨간색
         this.current = 'MODERNA'
+        if (item.includes("1차")) {
+          this.vaccine = 'MODERNA_FIRST'
+        } else {
+          this.vaccine = 'MODERNA_SECOND'
+        }
         this.borderColor = 'rgb(237, 98, 56)'
         this.backgroundColor = 'rgb(237, 98, 56)'
       }
-      
       else if (item.includes("아스트라제네카")) {
         // 흰색인데 기본색으로 표현
         this.current = 'AZ'
+        if (item.includes("1차")) {
+          this.vaccine = 'AZ_FIRST'
+        } else {
+          this.vaccine = 'AZ_SECOND'
+        }
         this.borderColor = 'rgb(98, 176, 182)'
         this.backgroundColor = 'rgb(98, 176, 182)'
       }
       else if (item.includes("얀센")) {
         // 파란색
         this.current = 'ANSEN'
+        if (item.includes("부스터샷")) {
+          this.vaccine = 'ANSEN_BOOST'
+        } else {
+          this.vaccine = 'ANSEN'
+        }
         this.borderColor = 'rgb(81, 134, 236)'
         this.backgroundColor = 'rgb(81, 134, 236)'
       }
@@ -144,7 +178,3 @@ export default {
   },
 }
 </script>
-
-<style scoped lang="scss">
-
-</style>
