3ade788c65fd1328dfa71bf32908a49eeaf1e3f9
신승민 <sky2197kr@gmail.com>
2021-12-04 20:22:10 +0900

    글쓰기 버튼 색 및 크기 변경

--- src/components/_Common/writeBtn.vue
+++ src/components/_Common/writeBtn.vue
@@ -4,21 +4,21 @@
       absolute
       bottom-3
       right-1/2
-      w-24
-      h-8
+      w-28
+      h-12
       rounded-full
       flex
       justify-center
       items-center
-      border border-primary
-      bg-white
-      text-primary
+      border-2 border-gray-300
+      bg-gray-100
       transform
       translate-x-1/2
+      text-lg
     "
     @click="clickWriteBtn"
   >
-    <fa-icon class="text-md mr-2" icon="pen" />
+    <fa-icon class="mr-2" icon="pen" />
     <div>{{ content }}</div>
   </button>
 </template>
