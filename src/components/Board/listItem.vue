83be51d8e44f2f3574cce3775a947c26176d198a
신승민 <sky2197kr@gmail.com>
2021-12-05 16:32:31 +0900

    board list  텍스트 길이 제한 및 css 변경

--- src/components/Board/listItem.vue
+++ src/components/Board/listItem.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="card shadow mx-2 mb-2 bg-white" @click="$emit('click')">
     <div class="card-body p-4">
-      <div class="flex justify-between align-middle mb-2">
-        <div class="text-sm font-bold">
+      <div class="flex justify-between align-top mb-2">
+        <div class="text-sm font-bold truncate">
           {{ title }}
         </div>
-        <div class="text-2xs flex items-center">
+        <div class="text-2xs text-right w-32">
           {{ mm_formatDate(date) }}
         </div>
       </div>
-      <p class="text-xs mb-2">
-        {{ content }}
+      <p class="text-sm mb-2">
+        {{ ellipsisContent }}
       </p>
       <div class="flex justify-between">
         <div class="text-2xs">
@@ -30,7 +30,7 @@ export default {
   props: {
     title: {
       type: String,
-      default: null,
+      default: '',
     },
     date: {
       type: String,
@@ -38,7 +38,7 @@ export default {
     },
     content: {
       type: String,
-      default: null,
+      default: '',
     },
     nickname: {
       type: String,
@@ -47,6 +47,12 @@ export default {
     commentCnt: {
       type: Number,
       default: 0,
+    },
+  },
+  computed: {
+    ellipsisContent () {
+      const textLimit = 100;
+      if (this.content.length > textLimit) { return this.content.substring(0, textLimit) + '...'; } else { return this.content; };
     },
   },
   methods: {},
