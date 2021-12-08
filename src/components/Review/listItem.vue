b727161ba62b5343b91cb12a2deffad0b3dc374b
YejiKim <sso06030@gmail.com>
2021-12-09 00:10:24 +0900

    eslint 수정

--- src/components/Review/listItem.vue
+++ src/components/Review/listItem.vue
@@ -1,82 +1,88 @@
 <template>
-  <div class="w-screen review-list overflow-y-scroll">
-    <div v-for="(item,idx) in getReviewList" :key="idx">
-      <div class="card shadow m-2 bg-white" @click="$emit('clickDetail',item)">
-        <div class="card-body p-4">
-          <div class="flex justify-between align-middle mb-2">
-            <div>
-              <div class="flex justify-start items-center">
-                <div class="flex justify-center items-center">
-                  <div
-                    class="badge badge-xs mr-2"
-                    :style="{backgroundColor: getColor(item.vaccine), border: 0}"
-                  />
-                  <div class="mr-2 text-lg font-semibold">
-                    {{ getVaccineName(item.vaccine) }}
+  <div>
+    <div class="w-screen review-list overflow-y-scroll">
+      <div v-for="(item,idx) in getReviewList" :key="idx">
+        <div class="card shadow m-2 bg-white" @click="$emit('clickDetail',item)">
+          <div class="card-body p-4">
+            <div class="flex justify-between align-middle mb-2">
+              <div>
+                <div class="flex justify-start items-center">
+                  <div class="flex justify-center items-center">
+                    <div
+                      class="badge badge-xs mr-2"
+                      :style="{backgroundColor: getColor(item.vaccine), border: 0}"
+                    />
+                    <div class="mr-2 text-lg font-semibold">
+                      {{ getVaccineName(item.vaccine) }}
+                    </div>
                   </div>
-                </div>
-                <div class="flex justify-start items-center bg-gray-100 rounded-lg px-2">
-                  <div class="text-2xs">
-                    {{ getAgeName(item.authorAge) }}
-                  </div>
-                  <div class="text-lg" style="margin-left: 3px; margin-right: 3px">
-                    ・
-                  </div>
-                  <div class="text-2xs">
-                    {{ getGenderName(item.authorGender) }}
-                  </div>
-                  <div class="text-lg" style="margin-left: 3px; margin-right: 3px">
-                    ・
-                  </div>
-                  <div class="text-2xs">
-                    {{ item.authorNickName }}
+                  <div class="flex justify-start items-center bg-gray-100 rounded-lg px-2">
+                    <div class="text-2xs">
+                      {{ getAgeName(item.authorAge) }}
+                    </div>
+                    <div class="text-lg" style="margin-left: 3px; margin-right: 3px">
+                      ・
+                    </div>
+                    <div class="text-2xs">
+                      {{ getGenderName(item.authorGender) }}
+                    </div>
+                    <div class="text-lg" style="margin-left: 3px; margin-right: 3px">
+                      ・
+                    </div>
+                    <div class="text-2xs">
+                      {{ item.authorNickName }}
+                    </div>
                   </div>
                 </div>
               </div>
+              <div class="text-2xs flex items-center">
+                {{ mm_formatDate(item.createdDate) }}
+              </div>
             </div>
-            <div class="text-2xs flex items-center">
-              {{ mm_formatDate(item.createdDate) }}
-            </div>
-          </div>
-          <div>
-            <div
-              v-if="item.haveDisease"
-              class="mr-1 mb-2 btn btn-outline btn-primary btn-sm"
-              style="background-color: white; color: #009485;
-                 border: 1px solid #009485"
-            >
-              기저질환
-            </div>
-            <div
-              v-for="(effec,i) in item.sideEffects"
-              v-show="i<4"
-              :key="i"
-              class="mr-2 mb-2 btn btn-outline btn-primary btn-sm"
-              style="background-color: white; color: #65C3C8;
+            <div>
+              <div
+                v-if="item.haveDisease"
+                class="mr-1 mb-2 btn btn-outline btn-primary btn-sm"
+                style="background-color: white; color: #65C3C8;
                  border: 1px solid #65C3C8"
-            >
-              {{ getSideEffectsName(effec) }}
-            </div>
-            <div
-              v-if="item.sideEffects.length>4"
-              class="mr-1 mb-2 btn btn-outline btn-primary btn-sm"
-              style="background-color: white; color: #65C3C8;
+              >
+                기저질환
+              </div>
+              <div
+                v-for="(effec,i) in item.sideEffects"
+                v-show="i<4"
+                :key="i"
+                class="mr-2 mb-2 btn btn-outline btn-primary btn-sm"
+                style="background-color: white; color: #65C3C8;
                  border: 1px solid #65C3C8"
-            >
-              ...
+              >
+                {{ getSideEffectsName(effec) }}
+              </div>
+              <div
+                v-if="item.sideEffects.length>4"
+                class="mr-1 mb-2 btn btn-outline btn-primary btn-sm"
+                style="background-color: white; color: #65C3C8;
+                 border: 1px solid #65C3C8"
+              >
+                ...
+              </div>
             </div>
+            <p v-show="item.detailDisc" class="text-sm ml-1 mt-1">
+              {{ item.detailDisc }}
+            </p>
           </div>
-          <p v-show="item.detailDisc" class="text-sm ml-1 mt-1">
-            {{ item.detailDisc }}
-          </p>
         </div>
       </div>
     </div>
+    <!--    <infinite-loading v-if="getReviewList.length" @infinite="scrolling">-->
+    <!--      <div slot="no-results" />-->
+    <!--      <div slot="no-more" />-->
+    <!--    </infinite-loading>-->
   </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'ListCont',
@@ -90,24 +96,37 @@ export default {
     }),
   },
   methods: {
+    // scrolling ($state) {
+    //   this.currentPage += 1;
+    //
+    //   this.$axios.get(`board/list?page=${this.currentPage}&search=${this.keyword}`)
+    //     .then((r) => {
+    //       if (r.data.content.length !== 0) {
+    //         this.boardList.push(...r.data.content);
+    //         $state.loaded();
+    //       } else {
+    //         $state.complete();
+    //       }
+    //     });
+    // },
     getColor (vaccine) {
       switch (vaccine) {
         case 'ANSEN':
         case 'ANSEN_BOOST':
-          return 'rgb(81, 134, 236)'
+          return 'rgb(81, 134, 236)';
         case 'PFIZER_FIRST':
         case 'PFIZER_SECOND':
-          return 'rgb(180, 110, 188)'
+          return 'rgb(180, 110, 188)';
         case 'MODERNA_FIRST':
         case 'MODERNA_SECOND':
-          return 'rgb(237, 98, 56)'
+          return 'rgb(237, 98, 56)';
         case 'AZ_FIRST':
         case 'AZ_SECOND':
-          return 'rgb(98, 176, 182)'
+          return 'rgb(98, 176, 182)';
       }
     },
   },
-}
+};
 </script>
 
 <style scoped>
