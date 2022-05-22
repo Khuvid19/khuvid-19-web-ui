2e9c50cf7b416942487036d80e42a5ea2e27816e
sso06030 <sso06030@gmail.com>
2022-05-22 15:16:35 +0900

    vue-gtag 버전 수정

--- src/plugins/vue-gtag.js
+++ src/plugins/vue-gtag.js
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 import VueGtag from 'vue-gtag';
 
-export default ({ app }) => {
-  Vue.use(VueGtag, {
-    config: { id: 'G-HQL9YKGFNQ' },
-  }, app.router);
-}
+Vue.use(VueGtag, {
+  config: { id: 'G-HQL9YKGFNQ' },
+});
